bd6dfa44daf9ce0ae849156a5eb5567b3c4cca31
Charles Strahan <charles.c.strahan@gmail.com>
2012-11-19 03:01:20 -0500

    add color-image target

--- src/sikuli_clj/target.clj
+++ src/sikuli_clj/target.clj
@@ -1,5 +1,6 @@
 (ns sikuli-clj.target
-  (:import [org.sikuli.api ImageTarget TextTarget]))
+  (:import [org.sikuli.api ColorImageTarget ImageTarget TextTarget]
+           [javax.imageio ImageIO]))
 
 (defn- try-parse-url
   [url]
@@ -17,3 +18,13 @@
     (ImageTarget. (or (try-parse-url arg)
                       (clojure.java.io/file arg)))
     (ImageTarget. arg)))
+
+(defn- read-img
+  [path]
+  (ImageIO/read (clojure.java.io/file path)))
+
+(defn color-image
+  [arg]
+  (if (string? arg)
+    (ColorImageTarget. (or (try-parse-url arg) (read-img arg)))
+    (ColorImageTarget. arg)))
